17619ef53d6db4d2d678a5a420485129ed3fd7e5
muhamd <muhamedahmed9099@gmail.com>
2023-01-12 02:26:02 +0200

    try sth

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -10,24 +10,25 @@ import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 function VideoDetail() {
   const { id } = useParams();
-  const [videoDetail, setVideoDetail] = useState(null);
-  const [videos, setVideos] = useState(null);
+  // const [videoDetail, setVideoDetail] = useState(null);
+  // const [videos, setVideos] = useState(null);
 
-  const {
-    snippet: { title, channelId, channelTitle },
-    statistics: { viewCount, likeCount },
-  } = videoDetail;
-  useEffect(() => {
-    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
-      setVideoDetail(data.items[0])
-    );
 
-    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
-      (data) => setVideos(data.items)
-    );
-  }, [id]);
+  // const {
+  //   snippet: { title, channelId, channelTitle },
+  //   statistics: { viewCount, likeCount },
+  // } = videoDetail;
+  // useEffect(() => {
+  //   fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+  //     setVideoDetail(data.items[0])
+  //   );
 
-  if(!videoDetail?.snippet) return <Loader />;
+  //   fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+  //     (data) => setVideos(data.items)
+  //   );
+  // }, [id]);
+
+  // if(!videoDetail?.snippet) return <Loader />;
 
   return (
     <Box minHeight="95vh">
@@ -35,10 +36,10 @@ function VideoDetail() {
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
-            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+            {/* <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
-            </Typography>
-            <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
+            </Typography> */}
+            {/* <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
               <Link to={`/channel/${channelId}`}>
                 <Typography variant={{ sm: "subtitle1", md: 'h6' }}  color="#fff" >
                   {channelTitle}
@@ -53,12 +54,12 @@ function VideoDetail() {
                   {parseInt(likeCount).toLocaleString()} likes
                 </Typography>
               </Stack>
-            </Stack>
+            </Stack> */}
           </Box>
         </Box>
-        <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
+        {/* <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
           <Videos videos={videos} direction="column" />
-        </Box>
+        </Box> */}
       </Stack>
     </Box>
   );
