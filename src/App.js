dd02c00e16d75beb28d72148be090d0337abf0a4
Muhamed <ahmdfrym@gamil.com>
2023-01-23 13:55:34 +0200

    done the form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/system'
 import {NavBar, VideoDetail, Feed, ChannelDetail, SearchFeed} from './components'
+import Form from './components/Form'
 function App() {
   return (
     <BrowserRouter>
         <Box sx={{background: '#000'}}>
             <NavBar />
+            <Form />
             <Routes>
                 <Route path='/' exact element={<Feed />} />
                 <Route path='/video/:id' element={<VideoDetail />} />
